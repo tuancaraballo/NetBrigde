239b5c0d52d6d151783810f15c504670dac1f93e
Antuan Caraballo <tranc2606@gmail.com>
2017-07-16 00:35:30 -0700

    working on firebase

--- app.js
+++ app.js
@@ -11,9 +11,63 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var mustacheExpress = require('mustache-express');
 
+var _ = require('underscore');
+var PORT = process.env.PORT || 3000;
+
+// --> dependency for firebase database
+var firebase = require("firebase");
+
+  var config = {
+    apiKey: "AIzaSyCopE3i8lSqk7bMZJBOml7uV223XExfYe8",
+    authDomain: "unagifinal.firebaseapp.com",
+    databaseURL: "https://unagifinal.firebaseio.com",
+    projectId: "unagifinal",
+    storageBucket: "",
+    messagingSenderId: "784732129608"
+  };
+firebase.initializeApp(config);
+
+
+// --> instantiating database
+var database = firebase.database();
+
+
 var app = express();
 
-// HERE IS MY NAME ANTUAN
+// -> Creates a new acccount in Firebase
+function createNewAccount(username, data, group = "student") {
+  firebase.database().ref(group+ '/' + username).set(data);
+}
+
+function authMiddleware(req, res, next) {
+	var user = firebase.auth().currentUser;
+	if( user !== null) {
+		//--> here you can read more data from db and put it into the
+		// the request
+		req.user = user;
+		next();
+	} else{
+		res.redirect('/login');
+	}
+}
+
+/* Schema for mentoMap 
+
+   mentorObject = {
+	   name: " ",
+	   email: " ",
+	  password: " ",
+       role: " ",
+     mentees: [ ],
+     gender: " ",
+     age: " ",
+     hometown: " "
+   } 
+*/
+
+
+
+var mentorMap = {};
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -40,19 +94,99 @@ app.get('/', function(req, res){
     res.render('index.html', {"yourname": "Modu"});
 });
 
-app.get('/login', function(req, res){
-    res.render('login.html', {"yourname": "Modu"});
+
+app.get('/login', function(req, res) {
+	return res.render('login.html', {"yourname": "Modu"});
+});
+
+app.post('/login', function(req, res){
+
+	var email = _.pick(req.body, 'email')['email'];
+	var password =  _.pick(req.body, 'password')['password'];
+
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+	
+		return res.redirect('/studentdashboard');
+	
+	}).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+
+  		return res.status(404).send({message: errorMessage});
+	});
+
+
+    //res.render('login.html', {"yourname": "Modu"});
 });
 
 app.get('/contactus', function(req, res){
     res.render('contactus.html', {"yourname": "Modu"});
 });
 
-app.get('/createAccount', function(req, res){
-    res.render('createAccount.html', {"yourname": "Modu"});
+// TODO: change to the right html to render
+app.get('/mentordashboard', function(req, res){
+	var response = { message: 'success' };
+	res.status(200).send(response);
+    // res.render('contactus.html', {"yourname": "Modu"});
+});
+
+// TODO: change to the right html to render
+app.get('/studentdashboard', function(req, res){
+	var response = { message: 'success' };
+	res.status(200).send(response);
+    // res.render('contactus.html', {"yourname": "Modu"});
 });
 
 
+// -> obtains te username from the user's email
+function getUsernameFromEmail(email){
+	console.log('Email inside the function -- ' + email);
+	var index = email.indexOf("@");
+	console.log('INDEX  ' + index);
+    var username= email.substring(0,index);
+    console.log('username   ' + username);
+    return username;
+
+}
+
+// -> creates a mentor's account
+app.post('/newmentor', function(req, res){
+	// TODO: do some data sanitazion before adding to database
+	var emailJSON = _.pick(req.body, 'email');
+	var username = getUsernameFromEmail(emailJSON['email']);
+	var mentorData = _.pick(req.body, 'fullname', 'email', 'password', 'role', 'gender', 'age', 'hometown');	
+	createNewAccount(username, mentorData, "mentors"); 
+	return res.redirect('/mentordashboard');
+});
+
+// -> creates a student's account
+app.post('/newstudent', function(req, res){
+	// TODO: do some data sanitazion before adding to database
+	var email = _.pick(req.body, 'email')['email'];
+	var password =  _.pick(req.body, 'password')['password'];
+	var username = getUsernameFromEmail(email);
+	var studentData = _.pick(req.body, 'fullname', 'email', 'gender', 'age', 'hometown', 'school', 'major');
+	createNewAccount(username, studentData, "students"); 
+	
+	firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+		return res.redirect('/studentdashboard');
+	}
+	).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+
+  		return res.status(404).send(errorMessage);
+	});
+
+	
+});
+
+
+app.listen(PORT, function() {
+		console.log('Server listering on port ' + PORT + ' ...');
+});
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   var err = new Error('Not Found');
@@ -71,4 +205,4 @@ app.get('/createAccount', function(req, res){
 //   res.render('error');
 // });
 
-module.exports = app;
+//module.exports = app;
